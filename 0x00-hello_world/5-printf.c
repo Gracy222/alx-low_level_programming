115f540b87902c3056d793bf10d5977491ff610d
gracy222 <gbamidele23@gmail.com>
2023-03-16 17:12:43 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,7 @@
  * Description: main - program to print out string
  *
  * Return: Always 0 (Success)
- * */
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
