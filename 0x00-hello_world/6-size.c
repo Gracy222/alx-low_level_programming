3a065a323ae1b97773644353ef474628f2736a64
gracy222 <gbamidele23@gmail.com>
2023-03-16 17:48:14 -0700

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - this is program to print the size of types
+ * Return: 0 if the program is successful
+ * */
+int main(void)
+{
+	char cahracter;
+	int integer;
+	long lg;
+	double dou;
+	float floating;
+
+	printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(character));
+	printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(integer));
+	printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(lg));
+	printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(dou));
+	printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(floating));
+
+
+
+	return (0);
+
+}
