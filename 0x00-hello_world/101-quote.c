9116947b5d47eaf963507809a6e80bc2b98b281b
gracy222 <gbamidele23@gmail.com>
2023-03-16 20:08:22 -0700

    task 7

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Ac program that print a line to the standard error
+ * Return: 1 (success)
+ * */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19"
