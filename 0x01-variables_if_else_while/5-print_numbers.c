ace494522afd8a2276ae0a0733806ff6084c603f
gracy222 <gbamidele23@gmail.com>
2023-03-17 20:23:00 -0700

    task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ *
+ */
+
+int main(void)
+{
+	char(letters);
+
+	for (letters = 0 ; letters < 10 ; letters++)
+		printf("%d", letters);
+	printf("\n");
+	return (0);
+}
