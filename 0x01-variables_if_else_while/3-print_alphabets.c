8819931623a9ab0e68a5aa2fba2e492840b24e94
gracy222 <gbamidele23@gmail.com>
2023-03-17 21:42:54 -0700

    task3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,9 +10,9 @@ int main(void)
 {
 	char letters;
 	
-	for (letters = 'a' ; letters <= 'z' ; letters++)
+	for (letters = 'a'; letters <= 'z'; letters++)
 		putchar(letters);
-	for (letters = 'A' ; letters <= 'Z'; letters++)
+	for (letters = 'A'; letters <= 'Z'; letters++)
 		putchar(letters);
 	putchar('\n');
 	return (0);
