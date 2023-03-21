8a74395307b47b9e0462e0775d1c4aeb9283d2b6
gracy222 <gbamidele23@gmail.com>
2023-03-21 15:11:20 -0700

    natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -13,13 +13,12 @@ int main(void)
 	int sum;
 	sum = 0;
 
+
 	for (i = 0 ; i < 10 ; i++)
-	{
 		if ((i % 3 == 0) || (i % 5 == 0))
 		{
 			sum = sum + i;
 		}
-	}
 	printf("%d\n", sum);
 	return (0);
 }
