e124373689b6576b77475a40a5de4e10762b2f5c
gracy222 <gbamidele23@gmail.com>
2023-03-21 11:21:53 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,34 +1,31 @@
 #include "main.h"
 
 /**
- * print_sign - function that print sign
+ * print_sign - function that prints sign
  *
  * @n: function parameter
  *
- * Return: 1, 0 and  -1
+ * Return: 1, 0 and -1
  */
 
 int print_sign(int n)
 {
 	if (n > 0)
 	{
+
 		_putchar('+');
-		_putchar(',');
-		_putchar(' ');
 		return (1);
+
 	}
 	else if (n == 0)
 	{
+
 		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
 		return (0);
 	}
 	else
 	{
 		_putchar('-');
-		_putchar(',');
-		_putchar(' ');
 		return (-1);
 	}
 }
