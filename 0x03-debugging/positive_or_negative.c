dd5aa24e84b8ba4e055f25ff7339ee06b05172f9
gracy222 <gbamidele23@gmail.com>
2023-03-22 01:00:04 -0700

    multiple main

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,16 +1,16 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * positive_or_negative - checks for positive or negative numbers
+ * positive_or_negative - checkes for positive or negative numbers
  *
  * @i: the number to be checked
+ *
  * Return: always 0
  */
-
 
 void positive_or_negative(int i)
 {
 	if (i < 0)
 		printf("%d is negative\n", i);
 	else
-		printf("%d is positive\n", i);
+		printf("%d is positive\n",i);
 }
