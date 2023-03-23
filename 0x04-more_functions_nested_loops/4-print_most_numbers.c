62014b19fc2bd6680af532e0d9f3d202970c0115
gracy222 <gbamidele23@gmail.com>
2023-03-23 14:16:22 -0700

    100-prime

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_numbers - checks for f digit (0 through 9).
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		if (a != 50)
+		{
+			if (a != 52)
+			{
+			_putchar(a);
+			}
+		}
+	}
+	_putchar('\n');
+}
