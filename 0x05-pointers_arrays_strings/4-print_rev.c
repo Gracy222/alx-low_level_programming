5f100ced59eb348c6a5dc3f3edd018ab57f8819a
gracy222 <gbamidele23@gmail.com>
2023-03-27 07:24:51 -0700

    4-print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - rint string in rev
+ * @s: string to be printed
+ */
+
+void print_rev(char *s)
+
+{
+	int a, b;
+
+	b = 0;
+
+	while (s[b] != '\0')
+		b++;
+	for (a = b - 1; a >= 0; a--)
+	{
+		_putchar(s[a]);
+	}
+	_putchar('\n');
+}
