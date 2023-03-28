06c2334c30f131484feac15423b1857c7e17b039
gracy222 <gbamidele23@gmail.com>
2023-03-27 18:33:07 -0700

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,11 +11,16 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i;
-	
 
-	for (i = 0 ; src[i] != '\0' ;i++)
-		dest[1] = src[i];
-	return (dest);
+	i = 0;
+	
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+		return (dest);
 }
 
 
