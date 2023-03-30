d67713b9e26a00a01548c050c548414ae3fbd4f3
gracy222 <gbamidele23@gmail.com>
2023-03-30 09:14:13 -0700

    print_numbers

--- 0x06-pointers_arrays_strings/101-main.c
+++ 0x06-pointers_arrays_strings/101-main.c
@@ -18,4 +18,5 @@ int main(void)
     print_number(-98);
     _putchar('\n');
     return (0);
+nclude "main.h"
 }
--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,19 +1,21 @@
-#include <stdio.h>
+#include "main.h"
+
 /**
- *
-
-int main(void)
+ * print_number -print number putchar
+ * @n:integer
+ * Return: void
+ */
+void print_number(int n)
 {
+	unsigned int x = n;
 
-	int n;
-	int a[5];
-	int *p;
-	
-	a[2] = 1024;
-	p = &n;
+	if (n < 0)
+	{
+		_putchar('-');
+		x = -x;
+	}
+	if ((x / 10) > 0)
+		print_number(x / 10);
 
-	*(p + 5) = 98;
-	/* ...so that this prints 98\n */
-	printf("a[2] = %d\n", a[2]);
-	return (0);
+	_putchar(x % 10 + '0');
 }
