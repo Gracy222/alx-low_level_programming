211fdf266610ff0247de34c764446feba8ba1a8d
gracy222 <gbamidele23@gmail.com>
2023-03-31 00:04:34 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - A c program that print a line to the standard error
- * Return 1 (success)
- * */
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
-
 }
