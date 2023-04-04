8a29d1d2b76bcca739e7b7a3314c406cb7bf9c88
gracy222 <gbamidele23@gmail.com>
2023-04-03 17:10:11 -0700

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strpbrk - that searches a string for any of a set of bytes.
+ * @s: parameter 1
+ * @accept: parameter 2
+ *
+ * Return: 0
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+
+	while (*s)
+	{
+	for (i = 0; accept[i]; i++)
+	{
+	if (*s == accept[i])
+	{
+		return (s);
+	}
+
+
+	}
+	s++;
+	}
+	return ('\0');
+}
+
