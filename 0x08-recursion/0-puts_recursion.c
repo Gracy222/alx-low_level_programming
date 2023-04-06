eff84fd2157c97e64bef1ef6e4b22066dd2fb71c
gracy222 <gbamidele23@gmail.com>
2023-04-05 21:21:25 -0700

    task0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
-/*
- * _puts_recursion - prints a string, followed by a new line.
- * @s: - parameter to be checked
- * Return: - Nothing
+/**
+ * _puts_recursion - print a string
+ * @s:string
+ * Return:void
  */
-
-void _puts_recursion(char *s);
+void _puts_recursion(char *s)
 {
-
-	if (*s != '\n')
+	if (*s)
 	{
-		putchar(*s);
+	_putchar(*s);
 		_puts_recursion(s + 1);
 	}
 	else
 	{
-		putchar('\n');
+		_putchar('\n');
 	}
+
+
 }
