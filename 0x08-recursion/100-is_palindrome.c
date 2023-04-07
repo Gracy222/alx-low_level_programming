9de6fd1c6ee5a65d40f831964b601b3196ab04bf
gracy222 <gbamidele23@gmail.com>
2023-04-06 21:39:06 -0700

    palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * check_palindrome - checks to see if a string is a palindrome
+ * @i:int
+ * @j:int
+ * @s: string
+ * Return: int
+ */
+int check_palindrome(int i, int j, char *s)
+{
+	if (i >= j)
+		return (1);
+	else if (s[i] != s[j])
+		return (0);
+	return (check_palindrome(i + 1, j - 1, s));
+}
+/**
+ * is_palindrome - states if a string is a palindrome
+ * @s:string
+ * Return: int
+ */
+int is_palindrome(char *s)
+{
+	int i;
+
+	i = _strlen_recursion(s) - 1;
+	return (check_palindrome(0, i, s));
+}
