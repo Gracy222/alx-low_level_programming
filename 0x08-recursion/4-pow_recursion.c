06075c19eca7af8bda5789aa87c5fb5c945d9a49
gracy222 <gbamidele23@gmail.com>
2023-04-06 18:23:11 -0700

    pow

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _pow_recursion – value of x raised
+ * @x:int
+ * @y:int
+ * Return:int
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+
+	{
+
+		return (-1);
+	}
+
+	else if (y != 0)
+
+		return (x * _pow_recursion(x, y - 1));
+
+	else
+	{
+
+
+		return (1);
+	}
+}
