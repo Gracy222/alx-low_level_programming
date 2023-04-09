ab0ef6b7676308ed2b17b4f384c26bae9779d83a
gracy222 <gbamidele23@gmail.com>
2023-04-09 14:10:00 -0700

    tasko

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
  * main - prints the number of arguments passed into it.
- *
+ * @argc: int
+ * @argv: int
  * Return: 0
  */
 int main(int argc, char *argv[]__attribute__((unused)))
