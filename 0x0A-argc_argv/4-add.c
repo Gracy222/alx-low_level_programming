464f3938284ce4951586195507e62bccaef6bf91
gracy222 <gbamidele23@gmail.com>
2023-04-09 21:37:53 -0700

    task4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positive numbers.
+ * @argc: int
+ * @argv: int
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, c;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	c = a + b;
+	{
+		printf("%d\n", c);
+			return (0);
+	}
+
+
+}
