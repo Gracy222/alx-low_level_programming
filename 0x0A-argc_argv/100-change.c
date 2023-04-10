a381f165edce0a283acf7cfa99dacf260cac357f
gracy222 <gbamidele23@gmail.com>
2023-04-09 22:10:28 -0700

    task5

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - prints the minimum number of coins
+ * @argc: int
+ * @argv: int
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int val, h;
+	int cash = 0;
+	char cent[5] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	val = atoi(argv[1]);
+
+	if (val < 0)
+		printf("%d\n", 0);
+	for (h = 0; h < 5; h++)
+	{
+		if (val >= cent[h])
+		{
+			cash += val / cent[h];
+			val = val % cent[h];
+		}
+	}
+	printf("%d\n", cash);
+	return (0);
+}
