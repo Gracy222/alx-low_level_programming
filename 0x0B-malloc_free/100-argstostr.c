5da409d922c61b3c80b70a01c5febedf10131fb8
gracy222 <gbamidele23@gmail.com>
2023-04-12 06:31:40 -0700

    task 5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,24 +1,39 @@
+#include <stdio.h>
 #include <stdlib.h>
-
 /**
- * main - counts the number of words in a string
- * @s: string
- * Return: int of number of words
+ **argstostr -  a function that concatenates all the arguments of your program.
+ *@ac : int
+ *@av : array
+ *Return: array
  */
-int wrdcnt(char *s)
+char *argstostr(int ac, char **av)
 {
-	int i, n = 0;
+	int i, j, len;
+	char *str;
 
-	for (i = 0; s[i]; i++)
+	if (ac == 0 || av == NULL)
+	return (NULL);
+
+	len = 0;
+	for (i = 0; i < ac; i++)
 	{
-		if (s[i] == ' ')
-		{
-			if (s[i + 1] != ' ' && s[i + 1] != '\0')
-				n++;
-		}
-		else if (i == 0)
-			n++;
+	for (j = 0; av[i][j]; j++)
+		len++;
+	len++;
 	}
-	n++;
-	return (n);
+
+	str = malloc(sizeof(char) * len);
+	if (str == NULL)
+	return (NULL);
+
+	len = 0;
+	for (i = 0; i < ac; i++)
+	{
+	for (j = 0; av[i][j]; j++)
+		str[len++] = av[i][j];
+	str[len++] = '\n';
+	}
+	str[len] = '\0';
+
+	return (str);
 }
