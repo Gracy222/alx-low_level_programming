f72ec724ddae0c046cb8eed1ca3e8bf86d2cb2c1
gracy222 <gbamidele23@gmail.com>
2023-04-12 07:12:34 -0700

    ttask 6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,30 +1,77 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
-#define DELIMITER " "
+#include "main.h"
 
 /**
- * strtow - splits a string into words
- * @str: string
- * Return: pointer to an array of strings
+ * count_word - helper function to count the number of words in a string
+ * @s: string to evaluate
+ *
+ * Return: number of words
+ */
+int count_word(char *s)
+{
+	int flag, c, w;
+
+	flag = 0;
+	w = 0;
+
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		if (s[c] == ' ')
+			flag = 0;
+		else if (flag == 0)
+		{
+			flag = 1;
+			w++;
+		}
+	}
+
+	return (w);
+}
+/**
+ * **strtow - splits a string into words
+ * @str: string to split
+ *
+ * Return: pointer to an array of strings (Success)
+ * or NULL (Error)
  */
 char **strtow(char *str)
 {
-	char **words = NULL;
-	char *word = NULL;
-	int i = 0;
+	char **matrix, *tmp;
+	int i, k = 0, len = 0, words, c = 0, start, end;
 
-	word = strtok(str, DELIMITER);
-		while (word != NULL)
+	while (*(str + len))
+		len++;
+	words = count_word(str);
+	if (words == 0)
+		return (NULL);
+
+	matrix = (char **) malloc(sizeof(char *) * (words + 1));
+	if (matrix == NULL)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
 	{
-	words = realloc(words, sizeof(char *) * (i + 1));
-	words[i] = word;
-	i++;
-	word = strtok(NULL, DELIMITER);
+		if (str[i] == ' ' || str[i] == '\0')
+		{
+			if (c)
+			{
+				end = i;
+				tmp = (char *) malloc(sizeof(char) * (c + 1));
+				if (tmp == NULL)
+					return (NULL);
+				while (start < end)
+					*tmp++ = str[start++];
+				*tmp = '\0';
+				matrix[k] = tmp - c;
+				k++;
+				c = 0;
+			}
+		}
+		else if (c++ == 0)
+			start = i;
 	}
-	words = realloc(words, sizeof(char *) * (i + 1));
-	words[i] = NULL;
 
-	return (words);
+	matrix[k] = NULL;
+
+	return (matrix);
 }
