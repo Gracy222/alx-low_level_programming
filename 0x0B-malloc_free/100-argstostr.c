886fd8eff69b02e6df3b452d5dbd0deb06009872
gracy222 <gbamidele23@gmail.com>
2023-04-11 20:40:57 -0700

    task 5

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+
+/**
+ * wrdcnt - counts the number of words in a string
+ * @s: string
+ * Return: int of number of words
+ */
+int wrdcnt(char *s)
+{
+	int i, n = 0;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] == ' ')
+		{
+			if (s[i + 1] != ' ' && s[i + 1] != '\0')
+				n++;
+		}
+		else if (i == 0)
+			n++;
+	}
+	n++;
+	return (n);
+}
