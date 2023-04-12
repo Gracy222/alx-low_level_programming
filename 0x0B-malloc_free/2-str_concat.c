a58d273cdd662596b5a55ec68ef3618530041d68
gracy222 <gbamidele23@gmail.com>
2023-04-11 20:08:58 -0700

    task2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,36 +3,38 @@
 #include <stdio.h>
 /**
  *  *str_concat - concatenates two strings.
- *  @s1: are pointers to two null-terminated strings
- *  that you want to concatenate.
- *  @s2: are pointers to two null-terminated strings
- *  that you want to concatenate.
+ *  @s1: string1
+ *  @s2: string2
  *
- *  Return: pointer should point to a newly allocated space in memory
- *  or NULL on failure
+ *  Return: char
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i = 0, j = 0;
-	char *s3;
+	unsigned int i, j, k, l;
+	char *s;
 
-	s3 = malloc(sizeof(s1) + sizeof(s2) + 1);
-
-	if (s3 == NULL)
+	if (s1 == NULL)
+		i = 0;
+	else
+	{
+		for (i = 0; s1[i]; i++)
+			;
+	}
+	if (s2 == NULL)
+		j = 0;
+	else
+	{
+		for (j = 0; s2[j]; j++)
+			;
+	}
+	k = i + j + 1;
+	s = malloc(k * sizeof(char));
+	if (s == NULL)
 		return (NULL);
-
-	while (s1[i] != '\0')
-	{
-		s3[i] = s1[i];
-		i++;
-	}
-	while (s2[j] != '\0')
-	{
-		s3[i] = s2[j];
-		i++;
-		j++;
-	}
-	s3[i] = '\0';
-
-	return (s3);
+	for (l = 0; l < i; l++)
+		s[l] = s1[l];
+	for (l = 0; l < j; l++)
+		s[l + i] = s2[l];
+	s[i + j] = '\0';
+	return (s);
 }
