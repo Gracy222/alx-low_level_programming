b5e54d50d1d5d84f44a328d9d2ae1334d73540a6
gracy222 <gbamidele23@gmail.com>
2023-04-19 11:44:05 -0700

    3-main

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,33 +1,40 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "3-calc.h"
 
 /**
- * main - simple operation performed
- * @argc: argument count
- * @argv: argument vector
+ * main - check the code for Holberton School students.
+ * @argc: argument count.
+ * @argv: argument vector.
  *
- * Return: int
+ * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
-	int a, b, c;
-	int (*fun)(int, int);
+	int a, b;
+	int (*operation)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-	fun = get_op_func(argv[2]);
-	if (fun == Null)
+
+	if (argv[2][1])
 	{
 		printf("Error\n");
-		exit(98);
+		exit(99);
 	}
-	c = fun(a, b);
-	printf("%d\n", c);
+
+	operation = get_op_func(argv[2]);
+
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	printf("%d\n", operation(a, b));
 	return (0);
 }
