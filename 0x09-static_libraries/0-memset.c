64dcfdc50fd369937f4e5c361e7bdc4dffb55cf2
Gracy222 <gbamidele23@gmail.com>
2023-06-13 05:29:22 -0700

    string

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_memset - fills memory with a constant byte.
+ *  @s: parameter 1
+ *  @b: parameter 2
+ *  @n: parameter 3
+ *
+ *  Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *i = s;
+
+	while (n--)
+	{
+	*i++ = b;
+	}
+	return (s);
+}
---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr- it locates a character in a string.
+ *
+ * @s: parameter 1
+ * @c: parameter 2
+ *
+ * Return: null
+ */
+char *_strchr(char *s, char c)
+{
+	int index;
+
+	for (index = 0; s[index] >= '\0'; index++)
+
+	{
+		if (s[index] == c)
+			return (s + index);
+
+	}
+	return ('\0');
+}
