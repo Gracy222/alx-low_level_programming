9665af334e754149cfb477f07dbcf90e5b328763
Gracy222 <gbamidele23@gmail.com>
2023-06-19 09:51:00 -0700

    Task 0

--- 0x0D-preprocessor/dog.h
+++ 0x0D-preprocessor/dog.h
@@ -1,21 +1,25 @@
-
 #ifndef DOG_H
 #define DOG_H
+
 /**
- *struct dog - a new type
- *@name : string
- *@age : float
- *@owner : string
- *Description: a new type that contains dog name, age, and owner.
+ * struct dog - A new type for representing dog.
+ * @name: The name of a dog.
+ * @age: The age of a dog.
+ * @owner: The name of a dog owner.
  */
-typedef struct dog
+
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
+
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
 #endif
